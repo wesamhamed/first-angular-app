4842131da0dffd815dbb81f80953704675653b31
wesamhamed <w.m.h.h2022@gmail.com>
2020-03-04 14:17:24 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'blog';
+  logo=`https://images.unsplash.com/photo-1583138081665-e4ccfb127c27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80`;
+  students: [
+    {
+      firstName: 'wesam',
+      lastName: 'hamed',
+      age: 25,
+    },
+    {
+      firstName: 'wesam',
+      lastName: 'hamed',
+      age: 25,
+    },
+    {
+      firstName: 'wesam',
+      lastName: 'hamed',
+      age: 25,
+    }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+import { PostComponent } from "./post/post.component";
+import { TagComponent } from "./tag/tag.component";
+import { CategoryComponent } from "./category/category.component";
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PostComponent,
+    TagComponent,
+    CategoryComponent,
+    CommentComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
